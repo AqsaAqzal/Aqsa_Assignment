44d56a541ae381c1223944644deb702e94b40b00
AqsaAqzal <aqsaafzal903@gmail.com>
2022-10-26 11:46:50 +0500

    added required depedencies

--- src/main/java/resources/InventoryResource.java
+++ src/main/java/resources/InventoryResource.java
@@ -1,4 +1,20 @@
 package resources;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+
+
+@Path("/inventory")
+
 public class InventoryResource {
+
+    @POST
+    @Path("/add")
+    public Response addItem() {
+
+    }
+
 }
